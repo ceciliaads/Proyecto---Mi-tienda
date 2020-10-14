40f570c6820aed2ae5a8dc0def37ce29cdfa6955
Cecilia Da Silva <ceciliadasilva@Cecilias-MacBook-Air.local>
2020-10-13 22:52:02 -0300

    Correccion de filtro de puntaje. Se agrego el filtro de categorias luego de ver a clase 23. Incluyendo, tambien un boton para limpiar checkboxs y busqueda

--- Maquetado/index.js
+++ Maquetado/index.js
@@ -1,15 +1,17 @@
 const filtroNombre = document.querySelector("#busqueda");
 const tarjetas = document.getElementsByClassName("producto");
-const filtroRating = document.getElementsByClassName("review-filter");
+const filtroRating = document.getElementsByClassName('review-filter');
+const filtroCategorias = document.getElementsByClassName('productos-categorias');
+const checkboxes = document.querySelectorAll(".review-filter")
+const botonLimpiar = document.getElementsByClassName("boton-limpiar")
  
 
+// FILTRO BUSQUEDA //
+
 filtroNombre.oninput = () => {
-  
   for (let tarjeta of tarjetas) {
-    
     const titulo = tarjeta.dataset.nombre;
     const busqueda = filtroNombre.value;
-    
     if (titulo.includes(busqueda)) {
       tarjeta.classList.remove('hidden');
     } else {
@@ -18,22 +20,92 @@ filtroNombre.oninput = () => {
   }
 };
 
+// FILTRO CATEGORIAS //
 
-for (let checkbox of filtroRating) {
-  
+for (let checkbox of filtroCategorias) {
   checkbox.onclick = () => {
-    for (let tarjeta of tarjetas) {
-      if (checkbox.checked) {
-        const rating = tarjeta.dataset.rating;
-        if (checkbox.value === rating) {
-          tarjeta.classList.remove('hidden');
-        } else {
-          tarjeta.classList.add('hidden');
-        }
-      } 
-      else {
+    filtrarTarjetasPorCategoria();
+  };
+}
+
+const categoriaSeleccionada = () => {
+  for (let checkbox of filtroCategorias) {
+    if (checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const coincidenCategoriaYTarjeta = tarjeta => {
+  const categorias = tarjeta.dataset.categoria;
+  for (let checkbox of filtroCategorias) {
+    if (checkbox.value === categorias && checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const filtrarTarjetasPorCategoria = () => {
+  for (let tarjeta of tarjetas) {
+    tarjeta.classList.add('hidden');
+    if (categoriaSeleccionada()) {
+      if (coincidenCategoriaYTarjeta(tarjeta)) {
         tarjeta.classList.remove('hidden');
       }
     }
+    else {
+      tarjeta.classList.remove('hidden')
+    }
+  }
+};
+
+
+// FILTRO RAITING ESTRELLAS //
+
+for (let checkbox of filtroRating) {
+  checkbox.onclick = () => {
+    filtrarTarjetas();
   };
+}
+
+const hayCheckboxSeleccionado = () => {
+  for (let checkbox of filtroRating) {
+    if (checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const coincidenCheckboxYTarjeta = tarjeta => {
+  const rating = tarjeta.dataset.rating;
+  for (let checkbox of filtroRating) {
+    if (checkbox.value === rating && checkbox.checked) {
+      return true;
+    }
+  }
+};
+
+const filtrarTarjetas = () => {
+  for (let tarjeta of tarjetas) {
+    tarjeta.classList.add('hidden');
+    if (hayCheckboxSeleccionado()) {
+      if (coincidenCheckboxYTarjeta(tarjeta)) {
+        tarjeta.classList.remove('hidden');
+      }
+    }
+    else {
+      tarjeta.classList.remove('hidden')
+    }
+  }
+};
+
+
+// BOTON LIMPIAR //
+
+botonLimpiar.onclick = () => {
+  filtroNombre.value = ""
+  for (let checkbox of checkboxes) {
+    checkbox.checked = false 
+  }
+  tarjeta.classList.remove('hidden')
 }
